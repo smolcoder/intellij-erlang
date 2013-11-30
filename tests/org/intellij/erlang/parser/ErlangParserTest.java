7987ce1a2c1499064f7a3aaaf24c3d6f6aff701b
Sergey Savenko <deadok22@gmail.com>
2013-11-30 15:08:34 +0400

    do not skip unresolved macro calls

--- tests/org/intellij/erlang/parser/ErlangParserTest.java
+++ tests/org/intellij/erlang/parser/ErlangParserTest.java
@@ -88,4 +88,5 @@ public class ErlangParserTest extends ErlangParserTestBase {
   public void testMacrosHacks()       { doTest(true, false); }
   public void testNullaryMacroSubst() { doTest(true, false); }
   public void testBinaryMacroSubst()  { doTest(true, false); }
+  public void testUnresolvedMacroIsNotSkipped() { doTest(true, false); }
 }
